4c95270dd17130161dad44d6d9f68b714ee4a0a6
erandesamdhan2003 <erandesamadhan2003@gmail.com>
2025-03-28 09:34:07 +0530

    Login Initail

--- frontend/src/components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -2,6 +2,16 @@ import React, { useState } from "react"
 
 export const ProfilePhotoSelector = ({image, setImage}) => {
     const [previewUrl, setPreviewUrl] = useState(null);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if(file) {
+            // update the image state
+            setImage(file);
+
+            // generate preview url from the file
+        }
+    }
     return (
         <div>
             Profile photo selector
