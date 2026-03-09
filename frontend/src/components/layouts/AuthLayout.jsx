2a2c042dac3d0354700402a0a6ce730acfc4b6f1
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2026-03-10 03:13:24 +0530

    test2

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -33,7 +33,7 @@ export const AuthLayout = ({ children }) => {
                         icon={<LuTrendingUpDown 
                             className='text-black my-auto ml-3 bg-purple-700 p-2 w-full h-auto rounded-full'
                         />}
-                        label="Hello Yash, Your Current Balance is"
+                        label="Track Your Income and Expenses"
                         value="420,000"
                         color="bg-primary"
                     />
